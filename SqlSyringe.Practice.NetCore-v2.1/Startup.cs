3ab903b75657136df393838ce4bc7d6d5ec2112a
Marcel <suterma@users.noreply.github.com>
2019-10-30 15:00:33 +0100

    added .net core practice projects

---
+++ SqlSyringe.Practice.NetCore-v2.1/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SqlSyringe.Practice.NetCore_v2._1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Enable SqlSyringe from a specific source IP only (will pass over request otherwise)
+            app.UseMiddleware<Syringe>(new InjectionOptions()
+            {
+                FromIp = IPAddress.Parse("::1") //Use your IP, e.g. ::1 is IPv6 localhost
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
